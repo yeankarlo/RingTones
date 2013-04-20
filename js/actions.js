bc9d8c3d381a306370262973765b36c3868dc7b4
Juan Carlos VO <yeankarlo@gmail.com>
2013-04-20 14:08:49 -0500

    File Download

--- js/actions.js
+++ js/actions.js
@@ -6,14 +6,31 @@ $(document).ready(function(e) {
 		$('#main ul li a').tap(function(){
 			//alert($(this).attr('rel'));
 			src=$(this).attr('rel');
-			$('#descargar').attr('title',$(this).text());
+			nom=$(this).text();
+			$('#descargar').attr('title',nom);
 		});
 		$('#descargar a').tap(function(){
 			if ($(this).text()=='Descargar'){
 				// Descarga archivos con transaction
+				var fileTransfer = new FileTransfer();
+				fileTransfer.download(
+					src,
+					// Envío a la tarjeta SD
+					//'file:///mnt/sdcard/ringtoneApp/'+nom+'.mp3',
+					
+					// Envío a la carpeta raíz del teléfono
+					'file:///mnt/ringtoneApp/'+nom+'.mp3',
+					function(entry) {
+						// Verificar que no exista el archiv en la carpeta
+						navigator.notification.alert("Archivo descargado.",null,"Completado","Ok");
+					},
+					function(error) {
+						navigator.notification.alert("Error en la descarga"+error.code,null,"Error","Ok");
+					}
+				);
 			}
 			if ($(this).text()=='Probar'){
-				// Play el archivo con MEDIA
+				// Reproduce el archivo con MEDIA de phonegap
 				my_media = new Media(src, null, function(){
 					alert('Error :-(');
 				});
